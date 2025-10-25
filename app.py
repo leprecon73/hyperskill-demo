2581fec3958912ae98bb8d87037d5c9a60a7af01
leprecon73 <41924221+leprecon73@users.noreply.github.com>
2025-10-25 05:55:46 +0400

    Initialize Flask application in app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import sys
+
+app = Flask(__name__)
+
+# write your code here
+
+
+# do not change the way you run the program
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        arg_host, arg_port = sys.argv[1].split(':')
+        app.run(host=arg_host, port=arg_port)
+    else:
+        app.run()
